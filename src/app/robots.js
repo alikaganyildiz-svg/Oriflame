ca1bf707fc6e498f00c989c34a1185e753955ec5
Caner Kuncu <warlordsw@gmail.com>
2026-02-20 19:16:55 +0300

    fix(seo): update base url to oriflamekatalogun.com for sitemap and robots

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || `https://${process.env.VERCEL_URL}` || 'http://localhost:3000';
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://oriflamekatalogun.com';
 
     return {
         rules: {
